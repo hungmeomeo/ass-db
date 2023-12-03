c28dfb383f48b3936d3f5727cf0d9df7fc82c5ca
hungmeomeo <hungdevhcmut@gmail.com>
2023-12-03 15:34:47 +0700

    Finish Q4

--- b/controller/Order.controller.js
+++ b/controller/Order.controller.js
@@ -1,22 +1,22 @@
 const sequelize = require("../database/sequelize");
 
 async function getOrders() {
-  {
-    try {
-      const [results, metadata] = await sequelize.query(`
+  try {
+    const [results, metadata] = await sequelize.query(`
       SELECT
       C.CustomerCode,
       C.FirstName AS CustomerFirstName,
       C.LastName AS CustomerLastName,
       C.Address AS CustomerAddress,
-      C.PhoneNumber AS CustomerPhoneNumber,
       F.CategoryCode,
       F.CategoryName AS FabricName,
+      CP.PhoneNumber,
       F.Color,
       F.Quantity,
       B.BoltCode,
       B.Length,
       O.OrderCode,
+      O.CancellationReason,
       O.OrderDate,
       O.TotalPrice,
       O.OrderStatus,
@@ -37,68 +37,76 @@ Employees E on E.EmployeeCode = O.EmployeeCode
       FabricCategories F ON B.CategoryCode = F.CategoryCode
     LEFT JOIN
       PaymentHistory P ON O.OrderCode = P.OrderCode
+    LEFT JOIN 
+      CustomerPhoneNumbers CP ON CP.CustomerCode = C.CustomerCode
     ORDER BY
-      C.CustomerCode, F.CategoryCode, O.OrderCode, P.PaymentDate
-          `);
+      C.CustomerCode, F.CategoryCode, O.OrderCode, P.PaymentDate`);
 
-      const customers = [];
-      let currentCustomer, currentCategory, currentOrder;
+    const customers = [];
+    let currentCustomer, currentCategory, currentOrder;
 
-      for (const result of results) {
-        if (
-          !currentCustomer ||
-          currentCustomer.CustomerCode !== result.CustomerCode
-        ) {
-          currentCustomer = {
-            CustomerCode: result.CustomerCode,
-            CustomerFirstName: result.CustomerFirstName,
-            CustomerLastName: result.CustomerLastName,
-            CustomerAddress: result.CustomerAddress,
-            CustomerPhoneNumber: result.CustomerPhoneNumber,
-            Categories: [],
-          };
-          customers.push(currentCustomer);
-        }
-
-        if (
-          !currentCategory ||
-          currentCategory.CategoryCode !== result.CategoryCode
-        ) {
-          currentCategory = {
-            CategoryCode: result.CategoryCode,
-            Orders: [],
-          };
-          currentCustomer.Categories.push(currentCategory);
-        }
-
-        if (!currentOrder || currentOrder.OrderCode !== result.OrderCode) {
-          currentOrder = {
-            CategoryCode: result.CategoryCode,
-            FabricName: result.FabricName,
-            Color: result.Color,
-            Quantity: result.Quantity,
-            OrderCode: result.OrderCode,
-            OrderDate: result.OrderDate,
-            TotalPrice: result.TotalPrice,
-            OrderStatus: result.OrderStatus,
-            PaymentDate: result.PaymentDate,
-            BoltCode: result.BoltCode,
-            Amount: result.Amount,
-            Length: result.Length,
-            EmployeeID: result.EmployeeCode,
-            EmployeeName: result.EmployeeName,
-          };
-          currentCategory.Orders.push(currentOrder);
+    for (const result of results) {
+      if (
+        !currentCustomer ||
+        currentCustomer.CustomerCode !== result.CustomerCode
+      ) {
+        currentCustomer = {
+          CustomerCode: result.CustomerCode,
+          CustomerFirstName: result.CustomerFirstName,
+          CustomerLastName: result.CustomerLastName,
+          CustomerAddress: result.CustomerAddress,
+          PhoneNumber: [result.PhoneNumber], // Initialize array with the first phone number
+          Categories: [],
+        };
+        customers.push(currentCustomer);
+      } else {
+        if (!currentCustomer.PhoneNumber.includes(result.PhoneNumber)) {
+          currentCustomer.PhoneNumber.push(result.PhoneNumber);
         }
       }
 
-      return customers;
-    } catch (error) {
-      console.error("Error retrieving customer data:", error);
-      throw error;
+      if (
+        !currentCategory ||
+        currentCategory.CategoryCode !== result.CategoryCode
+      ) {
+        currentCategory = {
+          CategoryCode: result.CategoryCode,
+          Orders: [],
+        };
+
+        currentCustomer.Categories.push(currentCategory);
+      }
+
+      if (!currentOrder || currentOrder.OrderCode !== result.OrderCode) {
+        currentOrder = {
+          CategoryCode: result.CategoryCode,
+          FabricName: result.FabricName,
+          Color: result.Color,
+          Quantity: result.Quantity,
+          BoltCode: result.BoltCode,
+          Amount: result.Amount,
+          Length: result.Length,
+          OrderCode: result.OrderCode,
+          OrderDate: result.OrderDate,
+          TotalPrice: result.TotalPrice,
+          OrderStatus: result.OrderStatus,
+          PaymentDate: result.PaymentDate,
+          CancelationReason: result.CancellationReason,
+          EmployeeID: result.EmployeeCode,
+          EmployeeName: result.EmployeeName,
+        };
+        currentCategory.Orders.push(currentOrder);
+      }
     }
+
+    return customers;
+  } catch (error) {
+    console.error("Error retrieving customer data:", error);
+    throw error;
   }
 }
+
+// Rest of the code remains unchanged
 
 async function getCategoriesByCustomerCode(customerCode) {
   try {
